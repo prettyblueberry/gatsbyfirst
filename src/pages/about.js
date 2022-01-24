4cf2ab5ed35a288274f622db4c30793d2de9bbdc
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2022-01-24 13:19:16 +0900

    using GatsbyLink: when you hover over the link, the resource will be already received.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 // Step 1: Import React
-import * as React from 'react';
+import * as React from 'react'
+import {Link} from 'gatsby'
 
 // Step 2: Define your component
 const AboutPage = () => {
     return (
         <main>
             <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
         </main>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 // step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 // step 2: Define your component
 const IndexPage = () => {
   return (
       <main>
         <h1>Welcome!</h1>
+          <Link to="/about">About</Link>
         <p>I'm making this by following the Gatsby Tutorial</p>
       </main>
   )
