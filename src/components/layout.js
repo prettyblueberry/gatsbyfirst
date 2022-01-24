aa868d48794285de2f6193db5d9ec1bd0e4904f7
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2022-01-24 17:11:32 +0900

    styling using *.module.css more detail

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,32 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container } from './layout.module.css';
+import {
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText
+} from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
     return (
         <div className={container}>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
