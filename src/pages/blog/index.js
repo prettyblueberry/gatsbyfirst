8a33aa4a3fad3d93c2632fbdbbf7a6ea1d82f3ab
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2022-01-26 20:52:07 +0900

    programmable routing

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
     return (
---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const BlogPost = ({ data, children }) => {
+    return (
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <p>{data.mdx.frontmatter.date}</p>
+            {children}
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+                date(formatString: "MMMM D, YYYY")
+            }
+        }
+    }
+`
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
+
+export default BlogPost
