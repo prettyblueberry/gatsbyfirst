36eedd4e6d32660b0aad3275dc9936c6dd7af9ef
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2022-01-26 22:05:58 +0900

    programmable routing link

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
@@ -10,7 +10,11 @@ const BlogPage = ({ data }) => {
                 {
                     data.allMdx.nodes.map((node) => (
                         <article key={node.id}>
-                            <h2>{node.frontmatter.title}</h2>
+                            <h2>
+                                <Link to={`/blog/${node.frontmatter.slug}`}>
+                                    {node.frontmatter.title}
+                                </Link>
+                            </h2>
                             <p>Posted: {node.frontmatter.date}</p>
                             <p>{node.excerpt}</p>
                         </article>
@@ -27,6 +31,7 @@ export const query = graphql`
                 frontmatter {
                     date(formatString: "MMMM D, YYYY")
                     title
+                    slug
                 }
                 id
                 excerpt
